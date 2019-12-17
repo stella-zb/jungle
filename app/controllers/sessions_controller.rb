6f64fdfddab592035a987b8b988ce3b4b70d9ce9
stella-zb <upperdaystudio@gmail.com>
2019-12-17 14:55:40 -0800

    use  authenticate_with_credentials method from user model, change the string to path in redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to root_path
     else
-      redirect_to '/login'
+      redirect_to login_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to login_path
   end
 end
