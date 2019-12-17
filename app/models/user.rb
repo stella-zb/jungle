704b1a56070bef858ae1a02bcde131940dd9595b
stella-zb <upperdaystudio@gmail.com>
2019-12-17 14:56:17 -0800

    add validation, downcase method, whitespace strip method, authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+
+  before_save :downcase_fields
+  def downcase_fields
+    self.name.downcase if self.name
+    self.email.downcase if self.email
+  end
+  
+  def strip_whitespace
+    self.email = self.email.strip if self.email
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase if email
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
